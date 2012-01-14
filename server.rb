1431a7d1f9dd28483a05809dd570f2f785eaaa87
Jonathan Werbell <jonathanwerbell@gmail.com>
2012-01-14 13:29:53 -0500

    working app

--- server.rb
+++ server.rb
@@ -20,19 +20,34 @@ class HelloWorld
     file = File.open("public#{file_name}", 'r')
     file.read()
   end
+  
+  def get_size(file_name)
+    File.size("public#{file_name}")
+  end
 
   def call(env)   
+    
+    ap env
+    
     file_name = env["REQUEST_PATH"]
     if file_name == "/"
       file_name = "/index.html"
+    elsif file_name == "/subfolder/"
+      file_name = "/subfolder/index.html"
     end
     
     file_path = "public" + file_name
     
     if FileTest.exists?(file_path)
-      [200, {"Content-Type" => "text/html"}, [get_content(file_name)]]
+      if file_path.match(/.*\.jpg/)
+        [200, {"Content-Type" => "image/jpeg", "Content-Length" => "#{get_size(file_name)}"}, [get_content(file_name)]]
+      elsif file_path.match(/.*\.html/)
+        [200, {"Content-Type" => "text/html", "Content-Length" => "#{get_size(file_name)}"}, [get_content(file_name)]]
+      elsif file_path.match(/.*\.mp4/)
+         [200, {"Content-Type" => "video/mp4", "Content-Length" => "#{get_size(file_name)}"}, [get_content(file_name)]]
+      end
     else
-      [404, {}, ["You have an error"]]
+      [404, {}, ["404 Not Found"]]
     end
   end
 end
